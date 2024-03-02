21a014d2f2f85e2916fa2043ee6f47e5c74dc9e5
Eddie Shin <eshinhw@gmail.com>
2024-03-02 15:53:52 -0500

    Create Navbar

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="flex bg-black h-24 align-center justify-start">
+      <span className="text-white text-2xl mx-44  my-auto">
+        My Stock Portfolio
+      </span>
+    </nav>
+  );
+}
+
+export default Navbar;
