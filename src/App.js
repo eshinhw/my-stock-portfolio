16bb543651b5f55d7da2b7f37b55fcc85c595309
Eddie Shin <eshinhw@gmail.com>
2024-03-02 15:54:30 -0500

    Add multiple components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+import Navbar from "./Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Setting from "./Setting";
+import AddStock from "./AddStock";
+import Portfolio from "./Portfolio";
+import Stats from "./Stats";
 
 function App() {
-  return <div className="App"></div>;
+  const [balance, setBalance] = useState(0);
+  const [timePeriod, setTimePeriod] = useState(1);
+  const [stocks, setStocks] = useState([]);
+  const [newStock, setNewStock] = useState("");
+  const [showPerf, setShowPerf] = useState(false);
+  useEffect(() => {
+    setShowPerf(false);
+  }, []);
+
+  return (
+    <body className="m-0">
+      <Navbar />
+      <Setting
+        balance={balance}
+        setBalance={setBalance}
+        timePeriod={timePeriod}
+        setTimePeriod={setTimePeriod}
+      />
+      <AddStock
+        stocks={stocks}
+        setStocks={setStocks}
+        newStock={newStock}
+        setNewStock={setNewStock}
+      />
+      <Portfolio stocks={stocks} setShowPerf={setShowPerf} />
+      <Stats showPerf={showPerf} />
+    </body>
+  );
 }
 
 export default App;
