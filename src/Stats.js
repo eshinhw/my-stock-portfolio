26f20c3461238279115e35a43853ba4fa62dfa24
Eddie Shin <eshinhw@gmail.com>
2024-03-03 10:05:37 -0500

    Add startYear and endYear

--- src/Stats.js
+++ src/Stats.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-function Stats({ showPerf }) {
+function Stats({ showPerf, startYear, endYear }) {
   return (
     <>
-      {console.log(showPerf)}
       {showPerf && (
         <div className="my-6 mx-44">
-          <span className="text-2xl font-bold">Historical Performance</span>
+          <span className="text-2xl font-bold">
+            Historical Performance ({startYear} - {endYear})
+          </span>
         </div>
       )}
     </>
