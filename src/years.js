c618d26db2cd0bc465d758434d8114b636f6a081
Eddie Shin <eshinhw@gmail.com>
2024-03-04 12:52:36 -0500

    Created years js helper function

---
+++ src/years.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function generateYears(startYear) {
+  var currentYear = new Date().getFullYear(),
+    years = [];
+  startYear = startYear || 1980;
+  while (startYear <= currentYear) {
+    years.push(startYear++);
+  }
+  return years;
+}
+
+export default generateYears;
