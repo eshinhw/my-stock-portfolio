af55875d2ab358f41b0fce4fcde2ae5087cd99b4
Eddie Shin <eshinhw@gmail.com>
2024-03-05 22:29:42 -0500

    Create Drawdown chart

---
+++ src/DrawdownChartWrapper.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Chart } from "react-google-charts";
+
+function DrawdownChartWrapper() {
+  const data = [
+    ["Date", "PV"],
+    ["2013", 1000],
+    ["2014", 1170],
+    ["2015", 660],
+    ["2016", 1030],
+  ];
+
+  const options = {
+    title: "Company Performance",
+    hAxis: { title: "Year", titleTextStyle: { color: "#333" } },
+    vAxis: { minValue: 0 },
+    chartArea: { width: "50%", height: "70%" },
+  };
+
+  return (
+    <Chart
+      chartType="AreaChart"
+      width="100%"
+      height="400px"
+      data={data}
+      options={options}
+    />
+  );
+}
+
+export default DrawdownChartWrapper;
