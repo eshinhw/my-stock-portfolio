f8475c00038fd9ba9b2aa38d46ae8e0110e69dc6
Eddie Shin <eshinhw@gmail.com>
2024-03-06 22:39:05 -0500

    Remove dummy data

--- src/PieChartWrapper.js
+++ src/PieChartWrapper.js
@@ -14,20 +14,6 @@ function PieChartWrapper({ assets }) {
     }
   }, []);
 
-  // const data = [
-  //   ["Symbol", "Weight"],
-  //   ["MMM", 50],
-  //   ["NVDA", 50],
-  // ];
-  // const data = [
-  //   ["Task", "Hours per Day"],
-  //   ["Work", 11],
-  //   ["Eat", 2],
-  //   ["Commute", 2],
-  //   ["Watch TV", 2],
-  //   ["Sleep", 7], // CSS-style declaration
-  // ];
-
   const options = {
     pieHole: 0.4,
     is3D: true,
